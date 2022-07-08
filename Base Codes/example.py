97c93d6506c66cb2513fc59449811d0767caaf71
Tyler Trimble <TylerL.Trimble@gmail.com>
2022-07-07 21:32:12 -0500

    Update Images

--- Base Codes/example.py
+++ Base Codes/example.py
@@ -6,16 +6,15 @@ import wdn_viz_003 as wdn
 import matplotlib.pyplot as plt
 model = wdn.initialize_model('CTown.inp')
 #%%
-# wdn.create_basic_plot(model, savefig=True, saveName = 'Basic')
-# wdn.create_demand_pattern_plot(model, savefig=True, saveName = 'DemandPatterns')
-# wdn.plot_distinct_nodes(model,parameter='elevation',binEdgeNum=5,savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap')
+wdn.create_basic_plot(model, savefig=True, saveName = 'Basic')
+wdn.plot_distinct_nodes(model,parameter='elevation',binEdgeNum=5,savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap')
 
-# wdn.plot_distinct_nodes(model,parameter='elevation',bins=[25,50,75,100,125,150],binSizeList = [100,125,150,175,200,225], binShapeList = ['.','v','8','D','1','p'], binBorderList=['b','k','y','b','k','y'],binBorderWidthList=[1,1.1,0.5,0.9,1,3],cmap=None,colorList=['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'],savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap2')
-# wdn.plot_continuous_links(model,parameter='flowrate',timestep=11,pumps=False,colorBarTitle="Flowrate", saveName="ContinuousFlowrate")
-# wdn.plot_distinct_nodes(model,parameter='pressure', timestep=3, binEdgeNum=5,savefig=True, legendTitle= 'Pressure at T', saveName = 'PressureMap')
-# wdn.plot_continuous_nodes(model,parameter='elevation',colorBarTitle="Elevation", saveName="ContinuousElevation")
-# wdn.plot_continuous_nodes(model,parameter='pressure',timestep=5,colorBarTitle="Elevation", saveName="ContinuousPressure")
-# wdn.plot_distinct_links(model,parameter='flowrate',timestep=5,pumps=False,bins=[-0.2,0.1,0.4,0.7,100],binEdgeNum=5,saveName="FlowRates")
+wdn.plot_distinct_nodes(model,parameter='elevation',bins=[25,50,75,100,125,150],binSizeList = [100,125,150,175,200,225], binShapeList = ['.','v','8','D','1','p'], binBorderList=['b','k','y','b','k','y'],binBorderWidthList=[1,1.1,0.5,0.9,1,3],cmap=None,colorList=['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'],savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap2')
+wdn.plot_continuous_links(model,parameter='flowrate',timestep=11,pumps=False,colorBarTitle="Flowrate", saveName="ContinuousFlowrate")
+wdn.plot_distinct_nodes(model,parameter='pressure', timestep=3, binEdgeNum=5,savefig=True, legendTitle= 'Pressure at T', saveName = 'PressureMap')
+wdn.plot_continuous_nodes(model,parameter='elevation',colorBarTitle="Elevation", saveName="ContinuousElevation")
+wdn.plot_continuous_nodes(model,parameter='pressure',timestep=5,colorBarTitle="Elevation", saveName="ContinuousPressure")
+wdn.plot_distinct_links(model,parameter='flowrate',timestep=5,pumps=False,bins=[-0.2,0.1,0.4,0.7,100],binEdgeNum=5,saveName="FlowRates")
 
 #%%
 #Animates pressure at all timesteps
@@ -25,7 +24,7 @@ wdn.animate_plot(model, wdn.plot_distinct_nodes,figsize=[24,14],parameter='press
 #%%
 
 #Plots red square with black outline at specific squares
-wdn.plot_distinct_nodes(model,parameter='elevation',binEdgeNum=5,savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap')
+wdn.plot_distinct_nodes(model,parameter='elevation',binEdgeNum=5,savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMapSpecial')
 wdn.draw_nodes(model,['J511','J411'],nodeSize=300,nodeColor='r',nodeShape='s',edgeColors='k',lineWidths=2)
 
 #%%
@@ -54,4 +53,4 @@ wdn.draw_base_elements(model,ax2)
 
 wdn.draw_legend(ax2,binNames)
 
-plt.show()
+wdn.save_fig(model, "Subplot")
