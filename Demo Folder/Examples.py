09159bd9083f0815265ba4deeb6cd022c752dcf9
Tyler Trimble <TylerL.Trimble@gmail.com>
2022-07-24 16:04:15 -0500

    Update demo folder

--- Demo Folder/Examples.py
+++ Demo Folder/Examples.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 #Runs simulations and stores necessary information about network and the simulation in a dataframe
 model = wdn.initialize_model('Networks\CTown.inp')
-
+model['image_path'] = 'D:\Class Projects\wdn-viz\Base Codes\Images'
 
 # In[3]:
 
@@ -29,15 +29,15 @@ wdn.plot_basic_elements(model,ax)
 
 #Plots links and pumps only without legend
 fig, ax = plt.subplots(figsize=(15,25))
-wdn.plot_basic_elements(model,ax,nodes=False,links=True,reservoirs=False,tanks=False,pumps=True,drawLegend=False)
+wdn.plot_basic_elements(model,ax,nodes=False,links=True,reservoirs=False,tanks=False,pumps=True,legend=False)
 
 
 # In[5]:
 
 
-#Plots basic elements but does not save the image. The save name can also be changed with saveName function input.
+#Plots basic elements but does not save the image. The save name can also be changed with save_name function input.
 fig, ax = plt.subplots(figsize=(15,25))
-wdn.plot_basic_elements(model,ax,savefig=False, saveName='Basic')
+wdn.plot_basic_elements(model,ax,savefig=False, save_name='Basic')
 
 
 # In[6]:
@@ -77,7 +77,7 @@ wdn.plot_discrete_nodes(model,ax,5,bins=[-0.0001,20,40,60,80],parameter='pressur
 
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots network elevation with all customization options used
-wdn.plot_discrete_nodes(model,ax,5,parameter='elevation',binSizeList = [100,125,150,175,200], binShapeList = ['.','v','8','D','1'], binBorderList=['b','k','y','b','k'],binBorderWidthList=[1,1.1,0.5,0.9,1],cmap=None,colorList=['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22'],savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap2')
+wdn.plot_discrete_nodes(model,ax,5,parameter='elevation',bin_size_list = [100,125,150,175,200], bin_shape_list = ['.','v','8','D','1'], bin_border_list=['b','k','y','b','k'],bin_border_width_list=[1,1.1,0.5,0.9,1],cmap=None,color_list=['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22'],savefig=True, legend_title= 'Elevation Groups', save_name = 'ElevationMap2')
 
 
 # In[11]:
@@ -85,7 +85,7 @@ wdn.plot_discrete_nodes(model,ax,5,parameter='elevation',binSizeList = [100,125,
 
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots network elevation continuously with color bar.
-wdn.plot_continuous_nodes(model,ax,parameter='elevation',colorBarTitle="Elevation", saveName="ContinuousElevation")
+wdn.plot_continuous_nodes(model,ax,parameter='elevation',color_bar_title="Elevation", save_name="ContinuousElevation")
 
 
 # In[12]:
@@ -93,7 +93,7 @@ wdn.plot_continuous_nodes(model,ax,parameter='elevation',colorBarTitle="Elevatio
 
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots mean network demand with autumn color map and node shape 'X' with node size 75
-wdn.plot_continuous_nodes(model,ax,parameter='demand',value='mean',cmap='autumn',nodeShape='X',nodeSize=75,colorBarTitle="Demand", saveName="ContinuousDemand")
+wdn.plot_continuous_nodes(model,ax,parameter='demand',value='mean',cmap='autumn',node_shape='X',node_size=75,color_bar_title="Demand", save_name="ContinuousDemand")
 
 
 # In[13]:
@@ -109,7 +109,7 @@ wdn.plot_discrete_links(model,ax,5,parameter='flowrate',value='mean',pumps=False
 
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots mean flowrate with 5 bin edges with custom bin edges, no reservoirs or tanks and custom cmap
-wdn.plot_discrete_links(model,ax,5,parameter='flowrate',value='mean',binWidthList=[1,1,2,2], reservoirs=False,tanks=False,cmap='autumn')
+wdn.plot_discrete_links(model,ax,5,parameter='flowrate',value='mean',bin_width_list=[1,1,2,2], reservoirs=False,tanks=False,cmap='autumn')
 
 
 # In[15]:
@@ -117,8 +117,8 @@ wdn.plot_discrete_links(model,ax,5,parameter='flowrate',value='mean',binWidthLis
 
 fig, ax = plt.subplots(figsize=(15,25))
 ax.set_facecolor((0.1, 0.1, 0.1))
-#Plots continuous mean flowrate, with minWidth and maxWidth being set equal to each other. Uses gray background to better see plot.
-wdn.plot_continuous_links(model,ax,parameter='flowrate',value='mean', cmap='bwr', pumps=False, minWidth=1,maxWidth=1)
+#Plots continuous mean flowrate, with min_width and max_width being set equal to each other. Uses gray background to better see plot.
+wdn.plot_continuous_links(model,ax,parameter='flowrate',value='mean', cmap='bwr', pumps=False, min_width=1,max_width=1)
 
 
 # In[16]:
@@ -126,8 +126,8 @@ wdn.plot_continuous_links(model,ax,parameter='flowrate',value='mean', cmap='bwr'
 
 fig, ax = plt.subplots(figsize=(15,25))
 ax.set_facecolor((0.1, 0.1, 0.1))
-#Plots continuous mean flowrate, with minWidth and maxWidth being set to different values. Uses gray background to better see plot.
-wdn.plot_continuous_links(model,ax,parameter='flowrate',value='mean', cmap='bwr', pumps=False, minWidth=1,maxWidth=3)
+#Plots continuous mean flowrate, with min_width and max_width being set to different values. Uses gray background to better see plot.
+wdn.plot_continuous_links(model,ax,parameter='flowrate',value='mean', cmap='bwr', pumps=False, min_width=1,max_width=3)
 
 
 # In[17]:
@@ -151,7 +151,7 @@ fig, ax = plt.subplots(figsize=(15,25))
 #Draws distinct elevation.
 wdn.plot_discrete_nodes(model,ax,5,parameter='elevation')
 #Plots red square with black outline at specific nodes
-wdn.draw_nodes(model,['J511','J411'],nodeSize=300,nodeColor='r',nodeShape='s',edgeColors='k',lineWidths=2)
+wdn.draw_nodes(model,['J511','J411'],node_size=300,node_color='r',node_shape='s',edge_colors='k',line_widths=2)
 
 
 # In[19]:
@@ -160,9 +160,9 @@ wdn.draw_nodes(model,['J511','J411'],nodeSize=300,nodeColor='r',nodeShape='s',ed
 #Combination of distinct nodes + continuous links
 fig, ax = plt.subplots(figsize=(15,25))
 ax.set_facecolor((0.1, 0.1, 0.1))
-wdn.plot_discrete_nodes(model,ax,5,parameter='elevation',savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap')
+wdn.plot_discrete_nodes(model,ax,5,parameter='elevation',savefig=True, legend_title= 'Elevation Groups', save_name = 'ElevationMap')
 
-wdn.plot_continuous_links(model,ax,parameter='flowrate',value=11,pumps=False,cmap='bwr',colorBarTitle="Flowrate", saveName="ContinuousFlowrate")
+wdn.plot_continuous_links(model,ax,parameter='flowrate',value=11,pumps=False,cmap='bwr',color_bar_title="Flowrate", save_name="ContinuousFlowrate")
 
 
 # In[20]:
@@ -172,9 +172,9 @@ wdn.plot_continuous_links(model,ax,parameter='flowrate',value=11,pumps=False,cma
 fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2,figsize=(25,25),sharex=True,sharey=True)
 ax2.set_facecolor((0.1, 0.1, 0.1))
 
-wdn.plot_discrete_nodes(model,ax1,5,parameter='elevation',savefig=True, legendTitle= 'Elevation Groups', saveName = 'ElevationMap')
+wdn.plot_discrete_nodes(model,ax1,5,parameter='elevation',savefig=True, legend_title= 'Elevation Groups', save_name = 'ElevationMap')
 
-wdn.plot_continuous_links(model,ax2,parameter='flowrate',value=11,pumps=False,cmap='bwr',colorBarTitle="Flowrate", saveName="ContinuousFlowrate")
+wdn.plot_continuous_links(model,ax2,parameter='flowrate',value=11,pumps=False,cmap='bwr',color_bar_title="Flowrate", save_name="ContinuousFlowrate")
 
 
 # In[21]:
@@ -205,9 +205,10 @@ wdn.plot_unique_data(model,ax,parameter='tag',cmap='tab10')
 
 
 model = wdn.initialize_model('Networks\\NWC_old_stations.inp')
+model['image_path'] = 'D:\Class Projects\wdn-viz\Base Codes\Images'
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots unique data from excel file
-wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameterType='link',dataType='unique',excelColumns=[0,1],reservoirs=False,tanks=False,valves=False)
+wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameter_type='link',data_type='unique',excel_columns=[0,1],reservoirs=False,tanks=False,valves=False)
 
 
 # In[25]:
@@ -215,7 +216,7 @@ wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameterType='link',da
 
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots continuous excel data
-wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameterType='link',dataType='continuous',excelColumns=[0,3],cmap='autumn_r',maxWidth=2)
+wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameter_type='link',data_type='continuous',excel_columns=[0,3],cmap='autumn_r',max_width=2)
 
 
 # In[26]:
@@ -223,7 +224,7 @@ wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameterType='link',da
 
 fig, ax = plt.subplots(figsize=(15,25))
 #Plots discrete excel data
-wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameterType='link',dataType='discrete',binEdgeNum=5,excelColumns=[0,3],cmap='autumn_r',maxWidth=2)
+wdn.plot_unique_data(model,ax,parameter='NWC Pipes.xlsx',parameter_type='link',data_type='discrete',bin_edge_num=5,excel_columns=[0,3],cmap='autumn_r',max_width=2)
 
 
 # In[27]:
@@ -239,7 +240,7 @@ for node in model['node_names']:
     data.append(random.randint(0,100))
     index.append(node)
     
-wdn.plot_unique_data(model,ax,parameter='custom_data',dataType='continuous',parameterType='node',customDataValues=[index,data],cmap='autumn')
+wdn.plot_unique_data(model,ax,parameter='custom_data',data_type='continuous',parameter_type='node',customDataValues=[index,data],cmap='autumn')
 
 
 # In[28]:
@@ -254,7 +255,7 @@ for node in model['node_names']:
     data.append(random.randint(0,100))
     index.append(node)
     
-wdn.plot_unique_data(model,ax,parameter='custom_data',dataType='discrete',binEdgeNum=5,parameterType='node',customDataValues=[index,data],cmap='autumn')
+wdn.plot_unique_data(model,ax,parameter='custom_data',data_type='discrete',bin_edge_num=5,parameter_type='node',customDataValues=[index,data],cmap='autumn')
 
 
 # In[29]:
@@ -262,5 +263,5 @@ wdn.plot_unique_data(model,ax,parameter='custom_data',dataType='discrete',binEdg
 
 #Animates pressure at all timesteps
 
-#wdn.animate_plot(model,ax,wdn.plot_distinct_nodes,figsize=[24,14],parameter='pressure',bins=[-25,0,25,50,75], binSizeList = [300,300,300,300,300,300],legendTitle= 'Pressure Groups', saveName = 'PresMap',savefig=False)
+#wdn.animate_plot(model,ax,wdn.plot_distinct_nodes,figsize=[24,14],parameter='pressure',bins=[-25,0,25,50,75], bin_size_list = [300,300,300,300,300,300],legend_title= 'Pressure Groups', save_name = 'PresMap',savefig=False)
 
