fe847be6123cef5adb08a71fe4d1ec18e3b3f3d3
Tyler Trimble <TylerL.Trimble@gmail.com>
2022-10-17 17:25:08 -0500

    Update reservoir and tank default style (see image in /Examples/Images

--- Examples/network_diameters.py
+++ Examples/network_diameters.py
@@ -1,48 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  8 17:18:55 2022
-
-@author: Tyler
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  8 16:36:38 2022
-
-@author: Tyler
-"""
 import visnet.network as visinit
 import visnet.drawing as visplot
 import matplotlib.pyplot as plt
 import os
 
-model = visinit.initialize_model(r'Networks\Net3.inp')
+# Run EPANET2.0 Simulation and store results
+model = visinit.initialize_model(r"Networks\Net3.inp")  
 
 cwd = os.getcwd()
-model['image_path'] = cwd + '\Images'
+# Sets image path to /Images folder
+model["image_path"] = cwd + "\Images"  
 
-fig, ax = plt.subplots(figsize=(10,15))
-plt.title("Pipe Diameters for CTown Network",fontsize=16)
+# Define figure to be drawn to
+fig, ax = plt.subplots(figsize=(10, 15))  
 
-ax.set_frame_on(False);
+# Sets title
+plt.title("Pipe Diameters for CTown Network", fontsize=16)  
 
-visplot.plot_unique_data(model, 
-                         ax, 
-                         parameter='diameter',
-                         unit='in',
-                         cmap='Blues',
-                         legend_loc_2='lower left',
-                         legend_title='Pipe Diameter (in)',
-                         font_color='k',
-                         legend_sig_figs=3,
-                         pump_color='mediumseagreen',
-                         pump_width=5,
-                         tank_shape='h',
-                         tank_color='b',
-                         tank_border_color='k',
-                         tank_border_width=2,
-                         reservoir_color='b',
-                         reservoir_size=150,
-                         reservoir_border_color='k',
-                         reservoir_border_width=3)
+# Disables frame around figure
+ax.set_frame_on(False)
+
+
+#
+visplot.plot_unique_data(
+    model, ax, 
+    parameter="diameter", 
+    unit="in", 
+    cmap="Blues",
+    bin_width_list=[1,1.4,1.8,2.2,2.6,3,3.4,4.2,5.4,7],
+    legend_loc_2="lower left", 
+    legend_title="Pipe Diameter (in)",
+    legend_sig_figs=0, 
+    pump_color="mediumseagreen", 
+    pump_width=5,
+    tank_shape="h", 
+    tank_color="b", 
+    tank_border_color="k", 
+    tank_border_width=2,  
+    reservoir_color="b", 
+    reservoir_size=150, 
+    reservoir_border_color="k",
+    reservoir_border_width=3,
+)
 plt.show()
