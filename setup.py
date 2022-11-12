33e8c9bf4ae458738ad73594ffa26a3d18b51f4d
Tyler Trimble <102005076+tylertrimble@users.noreply.github.com>
2022-11-11 22:34:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pandas==1.4.4',
+requirements = ['pandas',
                'numpy',
                'matplotlib',
                'wntr',
