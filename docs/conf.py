9ee9d7b7b789cd36de29083e0d9b247f8c46aff2
Tyler Trimble <102005076+tylertrimble@users.noreply.github.com>
2022-11-19 19:28:56 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'visnetwork'
+project = 'viswaternet'
 copyright = "2022, Tyler Trimble"
 author = "Tyler Trimble"
 
