1fc16e5a06db2a550df756790c665f3ca238b3ef
Meghna Thomas <74310847+meghnathomas@users.noreply.github.com>
2022-11-28 22:20:42 -0600

    Update example2a_network_diameters.py

--- Examples/example2a_network_diameters.py
+++ Examples/example2a_network_diameters.py
@@ -1,27 +1,31 @@
+"""
+This example demonstrates how VisWaterNet can be used to highlight the distinct pipe diameters present in a water distribution network by modifying both link color and width.
+"""
+
+# Import libraries
 import viswaternet as vis
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Run EPANET2.0 Simulation and store results
+# Initialize VisWaterNet model
 model = vis.VisWNModel(r"Networks/CTown.inp")  
 
-# Define figure to be drawn to
+# Initialize plot
 fig, ax = plt.subplots(figsize=(12, 12))  
 
-# Disables frame around figure
+# Hide frame on plot
 ax.set_frame_on(False)
 
-#Plots pipe diameters
+# Plot unique pipe diameters
 model.plot_unique_data(
-    ax, 
-    parameter="diameter", 
+    ax, parameter="diameter", 
     unit="in", 
     interval_link_width_list=np.linspace(1,7,10),
-    cmap="Blues", #Controls link widths
-    legend_loc_2="lower left", #Location of legend with bins
+    cmap="Blues", # Controls link widths
+    legend_loc_2="lower left", # Location of legend with bins
     legend_title="Pipe Diameter (in)",font_size=12,
-    legend_sig_figs=0, #Whole numbers only
-    pump_color="mediumseagreen", 
-    pump_width=5, #In Pixels
-)
+    legend_sig_figs=0, # Whole numbers only
+    pump_color="mediumseagreen", # Customize pump color and width
+    pump_width=5 # In Pixels )
+    
 plt.show()
