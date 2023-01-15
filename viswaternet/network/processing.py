b5674ec3ec248212b2a602fc9530b8bcbe98a552
Tyler Trimble <TylerL.Trimble@gmail.com>
2023-01-14 20:21:28 -0600

    Make it so num_intervals represents number of intervals and not number of interval edges

--- viswaternet/network/processing.py
+++ viswaternet/network/processing.py
@@ -330,7 +330,7 @@ def bin_parameter(
     
     if intervals == "automatic":
 
-        bins = num_intervals
+        bins = num_intervals - 1
 
         intervals = np.linspace(
             np.min(parameter_results), np.max(parameter_results), bins
