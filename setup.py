2b7c14e22997ed1f98ecdf6aa3fb38ba223715dd
Tyler Trimble <102005076+tylertrimble@users.noreply.github.com>
2023-06-23 15:20:08 -0500

    Update requirements to include imageio-ffmpeg

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ requirements = ['pandas',
                'matplotlib>=3.5.0',
                'wntr<=0.5.0',
                'imageio',
+               'imageio-ffmpeg',
                'networkx>=2.7']
 
 test_requirements = [ ]
