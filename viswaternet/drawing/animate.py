039817ac51d137ba53abbdb48df0a0ad42a4b497
Tyler Trimble <102005076+tylertrimble@users.noreply.github.com>
2024-04-12 00:06:55 -0500

    Add animate code to resize figure properly

--- viswaternet/drawing/animate.py
+++ viswaternet/drawing/animate.py
@@ -2,9 +2,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.tight_bbox as tight_bbox
+from matplotlib.transforms import Bbox, TransformedBbox, Affine2D
 import imageio
+import io
+import math
 from viswaternet.network import processing
 from viswaternet.utils import unit_conversion, convert_excel
+
+
 def animate_plot(
         self,
         function,
@@ -18,17 +24,17 @@ def animate_plot(
         **kwargs):
     """
     Builds .gif file animating network data across timesteps.
-    
+
     Arguments
     ---------
     ax : axes._subplots.AxesSubplot
         Matplotlib axes object.
-        
+
     function : viswaternet
         One of the general viswaternet drawing functions.
-        
+
         .. rubric:: Usable Functions
-        
+
         ================================= ==
             plot_basic_elements
             plot_discrete_nodes
@@ -37,28 +43,28 @@ def animate_plot(
             plot_continuous_links
             plot_unique_data
         ================================= ==
-        
+
     fps : integer
         Framerate that the .gif file will be generated with.
-        
+
     first_timestep : integer
         The starting timestep of the .gif file.
-        
+
     last_timestep : integer
         The last timestep of the .gif file.
-        
+
     unit : string
         The time unit that will be reported for each frame of the .gif file.
-        
+
         .. rubric:: Time Units
-        
+
         ====================  ====================================
         Default               :math:`s`
         min                   :math:`min`
         hr                    :math:`hr`
         day                   :math:`day`
         ====================  ====================================
-        
+
     kwargs : Any
         Any arguments for the plotting function passed into the function argument
         can be passed into animate_plot.
@@ -99,7 +105,7 @@ def animate_plot(
             kwargs["disable_interval_deleting"] = True
             if kwargs.get("intervals", None) is None:
                 kwargs["intervals"] = make_intervals(data_values, kwargs)
-        data_values = [data_values[i].tolist() 
+        data_values = [data_values[i].tolist()
                        for i in data_values.columns]
     else:
         timesteps = int(
@@ -143,11 +149,11 @@ def animate_plot(
                                                 custom_data_values[1][value]]
             except Exception:
                 kwargs["custom_data_values"] = [element_list,
-                                           data_values[value]]
+                                                data_values[value]]
                 kwargs["parameter"] = 'custom_data'
             function(ax=ax, savefig=False, **kwargs)
         else:
-            kwargs["value"] = [parameter_results.iloc[:,value],
+            kwargs["value"] = [parameter_results.iloc[:, value],
                                element_list]
             function(ax=ax, savefig=False, **kwargs)
         handles, labels = [], []
@@ -159,18 +165,21 @@ def animate_plot(
             title="Timestep "+str(time)+" "+time_unit,
             loc="lower left",
             frameon=False)
+        size_inches = fig.get_size_inches()
+        restore_bbox = bbox_inches_tight_resize(fig)
         fig.canvas.draw()
-        mat = np.array(fig.canvas.renderer._renderer)
+        with io.BytesIO() as buff:
+            fig.savefig(buff, format='raw')
+            buff.seek(0)
+            data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+            w, h = fig.canvas.get_width_height()
+            mat = data.reshape((int(h), int(w), -1))
         frames.append(mat)
-        try:
-            if function == self.plot_continuous_nodes \
-                    or function == self.plot_continuous_links \
-                    or data_type == 'continuous':
-                fig.axes[1].remove()
-        except Exception:
-            if function == self.plot_continuous_nodes \
-                    or function == self.plot_continuous_links:
-                fig.axes[1].remove()
+        restore_bbox()
+        if function == self.plot_continuous_nodes \
+                or function == self.plot_continuous_links \
+                or data_type == 'continuous':
+            fig.axes[1].remove()
         ax.clear()
     if save_format == "gif" or save_format == "GIF":
         imageio.mimsave(save_name+"."+save_format,
@@ -185,6 +194,7 @@ def animate_plot(
                         fps=fps,
                         quality=8,
                         ffmpeg_log_level='quiet')
+
 
 def make_vmin_vmax(parameter, kwargs):
     for value in np.min(parameter, axis=0):
@@ -212,3 +222,40 @@ def make_intervals(parameter, kwargs):
         np.max(np.max(parameter, axis=0), axis=0),
         kwargs.get("num_intervals", 5)).tolist()
     return intervals
+
+
+def bbox_inches_tight_resize(fig):
+    # Taken from matplotlib source code. This is how the bbox for
+    # saving figures with bbox_inches='tight' is done. Maybe there was a
+    # better way to do this? I don't know but tight_layout() wasn't doing
+    # the trick.
+    bbox_inches = fig.get_tightbbox()
+    bbox_artists = fig.get_default_bbox_extra_artists()
+    bbox_filtered = []
+    for a in bbox_artists:
+        bbox = a.get_window_extent()
+        if a.get_clip_on():
+            clip_box = a.get_clip_box()
+            if clip_box is not None:
+                bbox = Bbox.intersection(bbox, clip_box)
+            clip_path = a.get_clip_path()
+            if clip_path is not None and bbox is not None:
+                clip_path = clip_path.get_fully_transformed_path()
+                bbox = Bbox.intersection(bbox,
+                                         clip_path.get_extents())
+        if bbox is not None and (bbox.width != 0 or
+                                 bbox.height != 0):
+            if not np.isinf(bbox.width):
+                bbox_filtered.append(bbox)
+
+    if bbox_filtered:
+        _bbox = Bbox.union(bbox_filtered)
+        trans = Affine2D().scale(1.0 / fig.dpi)
+        bbox_extra = TransformedBbox(_bbox, trans)
+        bbox_inches = Bbox.union([bbox_inches, bbox_extra])
+
+    pad = 0.1
+    bbox_inches = bbox_inches.padded(pad)
+    restore_bbox = tight_bbox.adjust_bbox(fig, bbox_inches,
+                                          fig.canvas.fixed_dpi)
+    return restore_bbox
